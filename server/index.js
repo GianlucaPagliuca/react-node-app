1c67556a3e87399020fae0b2ce16f2e2561c2c5d
Gianluca Pagliuca <gianluca.pagliuca.93@gmail.com>
2022-11-16 07:41:26 +0000

    test

--- server/index.js
+++ server/index.js
@@ -5,8 +5,17 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get("/api/:cID", (req, res) => {
+    console.log(req.params.cID);
+    res.json({ message: `cID Passed in - ${req.params.cID}` });
+})
+
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from Express! works!" });
+});
+
+app.get("/test", (req, res) => {
+    res.json({ message: "New Route Works" });
 });
 
 app.listen(PORT, () => {
