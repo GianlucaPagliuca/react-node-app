86ac7f83fa930f045f29cbdb522e0a7c5d7ae4ce
Gianluca Pagliuca <gianluca.pagliuca.93@gmail.com>
2022-11-17 07:51:13 +0000

    Installed mongo db and have it query on connection

--- server/index.js
+++ server/index.js
@@ -1,9 +1,36 @@
 // server/index.js
 const express = require("express");
-
+const { MongoClient } = require("mongodb");
 const PORT = process.env.PORT || 3001;
 
+const uri = "mongodb://127.0.0.1/?maxPoolSize=20&w=majority";
 const app = express();
+
+const client = new MongoClient(uri);
+
+async function run(){
+    try{
+        // Connect the client to the server
+        await client.connect();
+
+        // Establish and verify connection
+        await client.db("admin").command({ping: 1});
+        console.log("Connected successfully to server");
+
+        // Queries the database when connected and outputs everything in the console
+        const result = await client.db("test").collection("test").find();
+        // Need to convert it to an array to be able to see what's inside
+        const resultFinal = await result.toArray();
+        console.log(resultFinal);
+
+        
+    }   finally{
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 app.get("/api/:cID", (req, res) => {
     console.log(req.params.cID);
